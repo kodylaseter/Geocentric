e7f7a2ab7814a36b1f6ec1bc93d328cc9eff8e8e
Collin Caldwell <Collin@Collins-MacBook-Pro.local>
2015-10-01 09:29:23 -0400

    Added folders to stay organized. Remade scripts I accedentally deleted

---
+++ Assets/Scripts/Player/ItemSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawn : MonoBehaviour {
+
+	public GameObject item;
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			Instantiate (item, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+		}
+	}
+}
