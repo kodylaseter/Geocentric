e224bfde1045a9006396cc0cc3a1d8b0a325f71a
Collin Caldwell <Collin@lawn-128-61-59-121.lawn.gatech.edu>
2015-10-02 13:18:18 -0400

    changed collider on rex

--- Assets/Scripts/LevelPortal.cs
+++ Assets/Scripts/LevelPortal.cs
@@ -6,7 +6,7 @@ public class LevelPortal : MonoBehaviour {
 	public string level;
 
 	void OnCollisionEnter (Collision col)
-	{	print (col.gameObject.tag);
+	{	
 		//Destroy(col.gameObject);
 		if (col.gameObject.tag.Equals ("Player")) {
 			Application.LoadLevel(level);
