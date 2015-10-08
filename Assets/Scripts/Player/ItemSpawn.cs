ba2a409d350770665b42b3448902e8b36084fe05
Collin caldwell <ccaldwell9@gatech.edu>
2015-10-07 23:04:37 -0400

    Add item changing functionality Add a GameObject array to your ItemSpawner GameObject. Can drag and drop items into the list to spawn them.

--- Assets/Scripts/Player/ItemSpawn.cs
+++ Assets/Scripts/Player/ItemSpawn.cs
@@ -3,37 +3,38 @@ using System.Collections;
 
 public class ItemSpawn : MonoBehaviour {
 
-	public GameObject item;
+	GameObject item;
+	public int itemNum = 0;
+	public GameObject[] items;
 	public Transform[] spawnPoints;
 	public int maxItems = 1000;
 	public int spawnedItems;
 	private bool m_isAxisInUse = false;
+	private bool m_placeAxisInUse = false;
 
 
 	// Use this for initialization
 	void Start () {
+		item = items [itemNum];
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (Input.GetKeyDown (KeyCode.R)) {
-//			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-//			Instantiate (item, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
-//			spawnedItems += 1;
-//			print (getSpawnedItems ());
-//		}
+
+		// Handles placing items
 		if( Input.GetAxisRaw("placeItem") != 0)
 		{	
 			if(m_isAxisInUse == false)
 			{
 				// Call your event function here.
-				if (spawnedItems < maxItems) {
+				if (spawnedItems < maxItems) 
+				{
 					int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 					Instantiate (item, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
 					spawnedItems += 1;
 				}
-
+				
 				m_isAxisInUse = true;
 			}
 		}
@@ -41,6 +42,76 @@ public class ItemSpawn : MonoBehaviour {
 		{
 			m_isAxisInUse = false;
 		} 
+
+		// Handles Changing Items
+
+		if( Input.GetAxisRaw("changeItem") != 0)
+		{	
+			if(m_placeAxisInUse == false)
+			{
+				// Call your event function here.
+				if (Input.GetAxisRaw ("changeItem") > 0) 
+				{
+					if (itemNum < items.Length - 1) 
+					{
+						itemNum += 1;
+					} else 
+					{
+						itemNum = 0;
+					}
+					item = items [itemNum];
+				} else if (Input.GetAxisRaw ("changeItem") < 0) {
+					if (itemNum > 0) 
+					{
+						itemNum -= 1;
+					} else 
+					{
+						itemNum = items.Length;
+					}
+					item = items[itemNum];
+				}
+				
+				m_placeAxisInUse = true;
+			}
+		}
+		if( Input.GetAxisRaw("changeItem") == 0)
+		{
+			m_placeAxisInUse = false;
+		} 
+
+		// Handles Changing Items
+
+
+
+//		if( Input.GetAxisRaw("changeItem") != 0)
+//		{	
+//			if(m_isAxisInUse == false)
+//			{
+//				// Call your event function here.
+//
+//				if (Input.GetAxisRaw ("changeItem") > 0) {
+//					if (itemNum < items.Length - 1) {
+//						itemNum += 1;
+//					} else {
+//						itemNum = 0;
+//					}
+//					item = items [itemNum];
+//				} else if (Input.GetAxisRaw ("changeItem") < 0) {
+//					if (itemNum > 0) {
+//						itemNum -= 1;
+//					} else {
+//						itemNum = items.Length;
+//					}
+//					item = items[itemNum];
+//				}
+//				
+//				m_isAxisInUse = true;
+//			}
+//		}
+//		if( Input.GetAxisRaw("changeItem") == 0)
+//		{
+//			m_isAxisInUse = false;
+//		} 
 	}
 
 	public int getSpawnedItems() {
