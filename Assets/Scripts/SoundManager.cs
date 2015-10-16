22b43299b524f5c8e27ac25a89456d901468061a
Collin caldwell <ccaldwell9@gatech.edu>
2015-10-16 18:18:14 -0400

    fix merge conflicts

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -9,12 +9,9 @@ public class SoundManager : MonoBehaviour{
 	public AudioClip[] defaultFootsteps;
 	public AudioClip[] obsidianFootsteps;
 	public AudioClip[] lavaFootsteps;
-<<<<<<< HEAD
 	public AudioClip[] collinGrass;
 	public AudioClip[] collinStone;
-=======
     public AudioClip[] musicBoxChordFootsteps;
->>>>>>> 57b1adebcc4b133fe6960e5c010715d2d3b6d954
 	
 	void Awake()
 	{
@@ -33,18 +30,13 @@ public class SoundManager : MonoBehaviour{
 			return obsidianFootsteps [Random.Range (0, obsidianFootsteps.Length)];
 		} else if (string.Compare (tag, "Lava") == 0) {
 			return lavaFootsteps [Random.Range (0, lavaFootsteps.Length)]; 
-<<<<<<< HEAD
 		} else if (string.Compare (tag, "collinStone") == 0) {
 			return collinStone [Random.Range (0, collinStone.Length)]; 
 		} else if (string.Compare (tag, "collinGrass") == 0) {
 			return collinGrass [Random.Range (0, collinGrass.Length)]; 
-		}
-		else return defaultFootsteps [Random.Range (0, defaultFootsteps.Length)];
-=======
 		} else if (string.Compare (tag, "musicBox") == 0) {
             return musicBoxChordFootsteps[Random.Range(0, musicBoxChordFootsteps.Length)];
         }
         else return defaultFootsteps [Random.Range (0, defaultFootsteps.Length)];
->>>>>>> 57b1adebcc4b133fe6960e5c010715d2d3b6d954
 	}
 }
