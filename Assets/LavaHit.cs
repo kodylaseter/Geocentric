c7acdcbab908ace40506b9ba573d8eb3d2eb20b9
Cora Wilson <coratatum@gmail.com>
2015-10-16 05:54:12 -0400

    Add lavadeath to my scene

---
+++ Assets/LavaHit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaHit : MonoBehaviour {
+	public AudioClip lavadeath;
+	AudioSource playerAudio;
+	ParticleSystem playerParticles;
+
+	// Use this for initialization
+	void Start () {
+		playerAudio = GetComponent<AudioSource> ();
+		playerParticles = GetComponentInChildren<ParticleSystem> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.tag.Equals("Lava")){
+			playerAudio.PlayOneShot(lavadeath);
+			GetComponent<mainCharacterScript>().Ragdoll();
+			playerParticles.Play();
+
+		}
+	}
+}
