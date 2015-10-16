16179e8fd6c664803515f3dd03f827f0b3fdad94
Cora Wilson <coratatum@gmail.com>
2015-10-16 18:18:44 -0400

    clean audio footstep code

--- Assets/Scripts/Player/AudioFootsteps.cs
+++ Assets/Scripts/Player/AudioFootsteps.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class AudioFootsteps : MonoBehaviour {
 
 	AudioSource playerAudio;
-	Animator anim;
-
 	string currentGroundTag = "Default";
 
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator> ();
 		playerAudio = GetComponent<AudioSource> ();
 	}
 
@@ -19,14 +16,13 @@ public class AudioFootsteps : MonoBehaviour {
 		RaycastHit hit;
 		if (Physics.Raycast (transform.position, Vector3.down, out hit)) {
 			currentGroundTag = hit.collider.tag;
+			print (hit.collider.gameObject.name);
 		}
-		print (currentGroundTag);
-		//requires that a SoundManager singleton exist for every level
+		//print (currentGroundTag);
 
 		if (currentGroundTag != "Item") {
-		playerAudio.clip = SoundManager.SM.GetSound (currentGroundTag);
-		playerAudio.Play ();
-			
+			playerAudio.clip = SoundManager.SM.GetSound (currentGroundTag);
+			playerAudio.Play ();
 		}
 
 	}
