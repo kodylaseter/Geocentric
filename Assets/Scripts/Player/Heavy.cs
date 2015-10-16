ac97e87f0c775329132f02b358d48810395f47a9
Monet Tomioka <monet.tomioka@gatech.edu>
2015-10-16 01:30:13 -0400

    Added more textures, changes to my scene

---
+++ Assets/Scripts/Player/Heavy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heavy : MonoBehaviour {
+    CapsuleCollider playerCollider;
+    GameObject rex;
+
+    void Awake() {
+        playerCollider = GetComponent<CapsuleCollider>();
+        rex = GameObject.Find("Rex");
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag.Equals("Snow"))
+        {
+            //print("hey");
+            rex.GetComponent<mainCharacterScript>().speed = 2;
+        }
+    }
+
+    void OnCollisionExit(Collision col) {
+        if (col.gameObject.tag.Equals("Snow"))
+        {
+            rex.GetComponent<mainCharacterScript>().speed = 15;
+        }
+    }
+}
