a304786472bdabc1a2324601e6c2ddf066a636bd
Cora Wilson <coratatum@gmail.com>
2015-10-16 21:38:44 -0400

    Change ground tag detection to feet colliders

---
+++ Assets/Scripts/Player/GroundTagScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Team Gemometry
+// Ben Seco, Collin Caldwell, Cora Wilson, Kody Laseter, Monet Tomioka
+
+public class GroundTagScript : MonoBehaviour {
+
+	// Use this for initialization
+	string currentGroundTag = "Default";
+
+	void OnTriggerEnter(Collider col)
+	{
+		currentGroundTag = col.gameObject.tag;
+		print ("GroundTagScript" + currentGroundTag);
+		//print(currentGroundTag);
+	}
+
+	public string GetGroundTag()
+	{
+		return currentGroundTag;
+	}
+}
