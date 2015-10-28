b2ee974cc929f253818de5ae423058ac55159397
Collin caldwell <ccaldwell9@gatech.edu>
2015-10-28 18:30:46 -0400

    add rain ai

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,10 +12,10 @@ public class SoundManager : MonoBehaviour{
 
 	Dictionary<string, List<AudioClip>> footstepTagMap;
 
-	void Start()
-	{
-		footstepTagMap = CreateFootstepTagMap ("Assets/Resources/Music/SoundEffects/Footstep");
-	}
+//	void Start()
+//	{
+//		footstepTagMap = CreateFootstepTagMap ("Assets/Resources/Music/SoundEffects/Footstep");
+//	}
 	
 	void Awake()
 	{
@@ -28,33 +28,33 @@ public class SoundManager : MonoBehaviour{
 		DontDestroyOnLoad (this);
 	}
 
-	Dictionary<string, List<AudioClip>> CreateFootstepTagMap(string resourceDirectory)
-	{
-		//placeholder empty dictionary to fill and return
-		Dictionary<string, List<AudioClip>> returnDict = new Dictionary<string, List<AudioClip>>{};
-
-		DirectoryInfo stepDir = new DirectoryInfo (resourceDirectory);
-		DirectoryInfo[] soundDirs = stepDir.GetDirectories ();
-		foreach (DirectoryInfo tagDir in soundDirs) {
-			//make sure to only get sound files
-			FileInfo[] soundFiles  = tagDir.GetFiles("*.wav");
-			//the tag to use in the project is the same as the directory name
-			string tag = tagDir.Name;
-
-			List<AudioClip> temp = new List<AudioClip>();
-			//for each clip in the folder, load the clip and add it to the list
-			foreach (FileInfo sound in soundFiles)
-			{
-				string soundName = sound.Name;
-				AudioClip audio = UnityEditor.AssetDatabase.LoadAssetAtPath(resourceDirectory+"/"+tag+"/"+soundName, typeof(AudioClip)) as AudioClip;
-				temp.Add(audio);
-			}
-			//add the tag and list entry to the dictionary
-			returnDict.Add(tag, temp);
-		}
-
-		return returnDict;
-	}
+//	Dictionary<string, List<AudioClip>> CreateFootstepTagMap(string resourceDirectory)
+//	{
+//		//placeholder empty dictionary to fill and return
+//		Dictionary<string, List<AudioClip>> returnDict = new Dictionary<string, List<AudioClip>>{};
+//
+//		DirectoryInfo stepDir = new DirectoryInfo (resourceDirectory);
+//		DirectoryInfo[] soundDirs = stepDir.GetDirectories ();
+//		foreach (DirectoryInfo tagDir in soundDirs) {
+//			//make sure to only get sound files
+//			FileInfo[] soundFiles  = tagDir.GetFiles("*.wav");
+//			//the tag to use in the project is the same as the directory name
+//			string tag = tagDir.Name;
+//
+//			List<AudioClip> temp = new List<AudioClip>();
+//			//for each clip in the folder, load the clip and add it to the list
+//			foreach (FileInfo sound in soundFiles)
+//			{
+//				string soundName = sound.Name;
+//				AudioClip audio = UnityEditor.AssetDatabase.LoadAssetAtPath(resourceDirectory+"/"+tag+"/"+soundName, typeof(AudioClip)) as AudioClip;
+//				temp.Add(audio);
+//			}
+//			//add the tag and list entry to the dictionary
+//			returnDict.Add(tag, temp);
+//		}
+//
+//		return returnDict;
+//	}
 
 	public AudioClip GetSound(string tag)
 	{
