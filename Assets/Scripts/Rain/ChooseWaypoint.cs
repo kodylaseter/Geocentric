2ed8ffce43710dde72784ac8735942f40e5027e4
kodylaseter <kodylaseter1@gmail.com>
2015-11-06 14:18:27 -0500

    updated startmenu added new scene with waypoint network

---
+++ Assets/Scripts/Rain/ChooseWaypoint.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+using RAIN.Representation;
+using RAIN.Motion;
+using RAIN.Navigation;
+using RAIN.Navigation.Graph;
+using RAIN.Navigation.Waypoints;
+
+[RAINAction("Choose Next Waypoint")]
+public class ChooseWaypoint : RAINAction
+{
+	public Expression WaypointNetwork = new Expression(); //either the name of the network or a variable containing a network
+	public Expression MoveTargetVariable = new Expression(); //the variable you want to use for the output move target
+	
+	private MoveLookTarget moveTarget = new MoveLookTarget();
+	private int lastWaypoint = -1;
+	private WaypointSet lastWaypointSet = null;
+	private static float count = 0f;
+
+	public override void Start(RAIN.Core.AI ai)
+	{
+		base.Start(ai);
+	}
+	
+	public override ActionResult Execute(AI ai)
+	{
+		count += 0.5f;
+		bool isNear = false;
+		if (ai.Motor.IsAt(moveTarget)){
+			isNear = true;
+		}
+		if ((moveTarget.VectorTarget == Vector3.zero) || isNear) {
+			isNear = false;
+			if (!MoveTargetVariable.IsValid || !MoveTargetVariable.IsVariable)
+				return ActionResult.FAILURE;
+			
+			WaypointSet waypointSet = GetWaypointSetFromExpression(ai);
+			if (waypointSet == null)
+				return ActionResult.FAILURE;
+			
+			if (waypointSet != lastWaypointSet)
+			{
+				lastWaypoint = -1;
+				lastWaypointSet = waypointSet;
+			}
+			
+			if (lastWaypoint == -1)
+			{
+				lastWaypoint = waypointSet.GetClosestWaypointIndex(ai.Kinematic.Position);
+				if (lastWaypoint < 0)
+					return ActionResult.FAILURE;
+				
+				moveTarget.VectorTarget = waypointSet.Waypoints[lastWaypoint].position;
+				moveTarget.CloseEnoughDistance = Mathf.Max(waypointSet.Waypoints[lastWaypoint].range, ai.Motor.CloseEnoughDistance);
+//				if (!ai.Motor.IsAt(moveTarget))
+//				{
+//					ai.WorkingMemory.SetItem<MoveLookTarget>(MoveTargetVariable.VariableName, moveTarget);
+//					return ActionResult.SUCCESS;
+//				}
+			}
+			
+			//**REMOVED EXTRA LINE HERE
+			NavigationGraphNode tNode = waypointSet.Graph.GetNode(lastWaypoint);
+			if (tNode.OutEdgeCount > 0)
+			{
+				int tRandomEdge = UnityEngine.Random.Range(0, tNode.OutEdgeCount); //**FIXED THIS LINE
+				lastWaypoint = ((VectorPathNode)tNode.EdgeOut(tRandomEdge).ToNode).NodeIndex;
+			}
+			moveTarget.VectorTarget = waypointSet.Waypoints[lastWaypoint].position;
+			moveTarget.CloseEnoughDistance = Mathf.Max(waypointSet.Waypoints[lastWaypoint].range, ai.Motor.CloseEnoughDistance);
+		}
+		ai.WorkingMemory.SetItem<MoveLookTarget>(MoveTargetVariable.VariableName, moveTarget);
+		if(count > 1000f)
+		{
+			ai.WorkingMemory.SetItem("isSearching", false);
+			count = 0;
+			return ActionResult.FAILURE;
+		}
+		return ActionResult.SUCCESS;
+		 
+	}
+	
+	private WaypointSet GetWaypointSetFromExpression(AI ai)
+	{
+		WaypointSet waypointSet = null;
+		
+		if (WaypointNetwork != null && WaypointNetwork.IsValid)
+		{
+			if (WaypointNetwork.IsVariable)
+			{
+				string varName = WaypointNetwork.VariableName;
+				if (ai.WorkingMemory.ItemExists(varName))
+				{
+					Type t = ai.WorkingMemory.GetItemType(varName);
+					if (t == typeof(WaypointRig) || t.IsSubclassOf(typeof(WaypointRig)))
+					{
+						WaypointRig wgComp = ai.WorkingMemory.GetItem<WaypointRig>(varName);
+						if (wgComp != null)
+							waypointSet = wgComp.WaypointSet;
+					}
+					else if (t == typeof(WaypointSet) || t.IsSubclassOf(typeof(WaypointSet)))
+					{
+						waypointSet = ai.WorkingMemory.GetItem<WaypointSet>(varName);
+					}
+					else if (t == typeof(GameObject))
+					{
+						GameObject go = ai.WorkingMemory.GetItem<GameObject>(varName);
+						if (go != null)
+						{
+							WaypointRig wgComp = go.GetComponentInChildren<WaypointRig>();
+							if (wgComp != null)
+								waypointSet = wgComp.WaypointSet;
+						}
+					}
+					else
+					{
+						string setName = ai.WorkingMemory.GetItem<string>(varName);
+						if (!string.IsNullOrEmpty(setName))
+							waypointSet = NavigationManager.Instance.GetWaypointSet(setName);
+					}
+				}
+				else
+				{
+					if (!string.IsNullOrEmpty(varName))
+						waypointSet = NavigationManager.Instance.GetWaypointSet(varName);
+				}
+			}
+			else if (WaypointNetwork.IsConstant)
+			{
+				waypointSet = NavigationManager.Instance.GetWaypointSet(WaypointNetwork.Evaluate<string>(0, ai.WorkingMemory));
+			}
+		}
+		
+		return waypointSet;
+	}
+}
---
+++ Assets/Scripts/StartMenu/MenuControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControl : MonoBehaviour {
+
+	private Vector3 levelSelectorPoint;
+	private Vector3 mainPoint;
+	private Transform cam;
+	private Transform currentPoint;
+	
+	void Start () {
+		levelSelectorPoint = GameObject.Find("levelSelectPoint").transform.position;
+		mainPoint = GameObject.Find("mainPoint").transform.position;
+		cam = GameObject.Find("Camera").transform;
+		currentPoint.position = mainPoint;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (currentPoint.position == levelSelectorPoint) {
+			int x = 1;
+		}
+		cam.LookAt(currentPoint);
+	}
+	
+	void OnMouseOver() {
+		string objName = transform.name;
+		TextMesh tm = GetComponent<TextMesh>();
+		tm.color = Color.gray;
+		if( Input.GetAxisRaw("placeItem") != 0) {
+			switch (objName) {
+				case "Start_Game_Text": Application.LoadLevel("Kody"); break;
+				case "Level_Select_Text": currentPoint.position = levelSelectorPoint; break;
+			}
+		}
+	}
+	
+	void OnMouseExit() {
+		GetComponent<TextMesh>().color = Color.white;
+	}
+}
