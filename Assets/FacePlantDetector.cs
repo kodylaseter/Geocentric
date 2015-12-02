0e5b8340c51ec4cf871da1612135b1bf29b9a025
Cora Wilson <coratatum@gmail.com>
2015-12-02 16:58:30 -0500

    No more faceplant
    :)

---
+++ Assets/FacePlantDetector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacePlantDetector : MonoBehaviour {
+
+	// Use this for initialization
+    bool isFaceplant = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        isFaceplant = true;
+       // print("FACEPLANT:" + isFaceplant);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        isFaceplant = false;
+       // print("FACEPLANT:" + isFaceplant);
+    }
+
+    public bool GetFaceplant()
+    {
+        return isFaceplant;
+    }
+}
--- Assets/Scripts/Player/GroundTagScript.cs
+++ Assets/Scripts/Player/GroundTagScript.cs
@@ -8,16 +8,30 @@ public class GroundTagScript : MonoBehaviour {
 
 	// Use this for initialization
 	string currentGroundTag = "Default";
+    bool isGrounded = true;
 
 	void OnTriggerEnter(Collider col)
 	{
 		currentGroundTag = col.gameObject.tag;
 //		print ("GroundTagScript" + currentGroundTag);
 		//print(currentGroundTag);
+        isGrounded = true;
+        //print(isGrounded);
 	}
+
+    void OnTriggerExit(Collider col)
+    {
+        isGrounded = false;
+        //print(isGrounded);
+    }
 
 	public string GetGroundTag()
 	{
 		return currentGroundTag;
 	}
+
+    public bool GetGrounded()
+    {
+        return isGrounded;
+    }
 }
