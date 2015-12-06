543e95ecb3aa852e536944229237c147bf813401
ccaldwell9 <ccaldwell9@gatech.edu>
2015-12-06 17:32:47 -0500

    Finished camera control in golf level

--- Assets/Scripts/golfStateScript.cs
+++ Assets/Scripts/golfStateScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class golfStateScript : MonoBehaviour {
 
 	Camera cam;
+
 	Animator anim; 
 	Transform holeCameraPos;
 	Transform look1;
@@ -12,11 +13,16 @@ public class golfStateScript : MonoBehaviour {
 	Transform lastCamera;
 	GameObject door;
 	Transform doorPos;
+	Transform levelCompleteCamera;
+
+	GameObject Rex;
+	GameObject goldenThing;	
 
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main;
 		anim = GetComponent <Animator> ();
+		levelCompleteCamera = GameObject.Find ("LevelCompleteCamera").transform;
 	}
 	
 	// Update is called once per frame
@@ -30,9 +36,23 @@ public class golfStateScript : MonoBehaviour {
 			if ( (Vector3.Distance(cam.transform.position,holeCameraPos.position)) < 1) {
 
 				DoorMover ();
-
 			}
+		}
 
+		if (animState.IsName ("LevelComplete")) {
+
+			cam.transform.position = Vector3.Lerp (cam.transform.position, levelCompleteCamera.position, .03f);
+			cam.transform.forward = Vector3.Slerp (cam.transform.forward, levelCompleteCamera.forward, .03f);
+			cam.transform.position = Vector3.Lerp (cam.transform.position, levelCompleteCamera.position, .03f);
+			goldenThing.transform.position = Vector3.Lerp (goldenThing.transform.position, Rex.transform.position+(Vector3.up*6),.03f);
+		}
+
+		if (animState.IsName ("SwitchLevel")) {
+			GameObject.Find ("LevelCompleteText").GetComponent<CanvasGroup> ().alpha += .05f;
+			if (Input.anyKey) {
+				
+				Application.LoadLevel(GameObject.Find ("Portal").GetComponent<LevelPortal>().level);
+			}
 		}
 	}
 	public void buttonPressed() {
@@ -54,7 +74,7 @@ public class golfStateScript : MonoBehaviour {
 	}
 
 	public void DoorMover() {
-		door.transform.position = Vector3.Lerp (door.transform.position, doorPos.position,.01f);
+		door.transform.position = Vector3.Lerp (door.transform.position, doorPos.position,.01f); 
 		if ( (Vector3.Distance(door.transform.position,doorPos.position)) < 30) {
 			print ("NEXT");
 			if (Input.anyKey) {
@@ -64,6 +84,16 @@ public class golfStateScript : MonoBehaviour {
 			
 		}
 
+
+	}
+
+	public void LevelComplete() {
+		anim.SetTrigger("LevelComplete");
+		Rex = GameObject.Find ("Rex");
+		GameObject.Find ("Rex").GetComponent<Animator> ().SetTrigger ("LevelComplete");
+		GameObject.Find ("Rex").GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+		GameObject.Find ("Portal").GetComponent<Collider> ().enabled = false;
+		goldenThing = GameObject.Find ("Golden Square");
 
 	}
 
